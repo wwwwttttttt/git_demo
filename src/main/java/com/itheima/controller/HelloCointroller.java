490d84fe9e7391a3bdfacf69d80d39d1351bd71d
Wt <1284305955@qq.com>
2023-09-05 16:51:33 +0800

    first commit

---
+++ src/main/java/com/itheima/controller/HelloCointroller.java
@@ -0,0 +1,11 @@
+package com.itheima.controller;
+
+/**
+ * @author WT
+ * @date : 2023/9/5
+ */
+public class HelloCointroller {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
