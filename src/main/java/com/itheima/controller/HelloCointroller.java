47b6fbe12780a52c43ffc620357a876311df9f64
Wt <1284305955@qq.com>
2023-09-05 17:50:59 +0800

    push commit

--- src/main/java/com/itheima/controller/HelloCointroller.java
+++ src/main/java/com/itheima/controller/HelloCointroller.java
@@ -11,6 +11,6 @@ public class HelloCointroller {
         System.out.println("hello git3");
         System.out.println("hello master");
         System.out.println("hello hot-fix");
-        System.out.println("hello push");
+        System.out.println("hello push1");
     }
 }
