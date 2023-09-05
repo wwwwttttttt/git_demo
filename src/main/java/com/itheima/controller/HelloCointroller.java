79bd2418e69c24812e0a6ffb189a928acf172a87
Wt <1284305955@qq.com>
2023-09-05 17:03:45 +0800

    second commit

--- src/main/java/com/itheima/controller/HelloCointroller.java
+++ src/main/java/com/itheima/controller/HelloCointroller.java
@@ -7,5 +7,6 @@ package com.itheima.controller;
 public class HelloCointroller {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
