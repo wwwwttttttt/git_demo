2fba4d48505a3bcc7183aa6d4335c55b4666460a
Wt <1284305955@qq.com>
2023-09-05 17:15:44 +0800

    hot-fix commit

--- src/main/java/com/itheima/controller/HelloCointroller.java
+++ src/main/java/com/itheima/controller/HelloCointroller.java
@@ -8,5 +8,6 @@ public class HelloCointroller {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
